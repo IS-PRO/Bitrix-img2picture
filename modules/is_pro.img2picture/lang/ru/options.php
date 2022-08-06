8e984fd167f6b11faaba79e2b6fcda54821e4805
Ivan Shabanov <ivan.shabanov@gmail.com>
2022-08-06 15:40:58 +0700

    feat: clear cache + reset options

--- modules/is_pro.img2picture/lang/ru/options.php
+++ modules/is_pro.img2picture/lang/ru/options.php
@@ -1,25 +1,33 @@
 <?
 $MESS['ISPRO_IMG2PICTURE_TAB_SET_OPTION'] = 'Настройки Img2Picture';
-$MESS['ISPRO_IMG2PICTURE_TAB_TITLE_OPTION'] = 'Настрйоки';
-$MESS['ISPRO_ING2PICTURE_RESPONSIVE'] = 'Адаптив';
-$MESS['ISPRO_ING2PICTURE_MIN_SCREEN_WIDTH'] = 'Минимальная ширина экрана экрана (min-width), px';
-$MESS['ISPRO_ING2PICTURE_MAX_SCREEN_WIDTH'] = 'Максимальная ширина экрана экрана (max-width), px';
-$MESS['ISPRO_ING2PICTURE_MAX_IMG_WIDTH'] = 'Ширина изображения px';
-$MESS['ISPRO_ING2PICTURE_TEMPLATE'] = 'Шаблон замены тега img';
-$MESS['ISPRO_ING2PICTURE_USE_WEBP'] = 'Применить конвертацию изображений в webp';
-$MESS['ISPRO_ING2PICTURE_IMG_COMPRESSION'] = 'Качество сохряняемых изображений (0-100)';
-$MESS['ISPRO_ING2PICTURE_EXCEPTIONS_DIR'] = 'Исключения: каталоги (директории) сайта, где не будет раотать модуль';
-$MESS['ISPRO_ING2PICTURE_EXCEPTIONS_SRC'] = 'Исключения: регулярные выражения для ссылок на изображения, которые не надо преобразовывать';
-$MESS['ISPRO_ING2PICTURE_MODULE_MODE'] = 'Режим работы модуля';
-$MESS['ISPRO_ING2PICTURE_MODULE_MODE_off'] = 'Выключен';
-$MESS['ISPRO_ING2PICTURE_MODULE_MODE_test'] = 'Тестирование';
-$MESS['ISPRO_ING2PICTURE_MODULE_MODE_on'] = 'Включен';
-$MESS['ISPRO_ING2PICTURE_SAVE'] = 'Сохранить';
+$MESS['ISPRO_IMG2PICTURE_TAB_TITLE_OPTION'] = 'Настройки';
+$MESS['ISPRO_IMG2PICTURE_RESPONSIVE'] = 'Адаптив';
+$MESS['ISPRO_IMG2PICTURE_MIN_SCREEN_WIDTH'] = 'Минимальная ширина экрана экрана (min-width), px';
+$MESS['ISPRO_IMG2PICTURE_MAX_SCREEN_WIDTH'] = 'Максимальная ширина экрана экрана (max-width), px';
+$MESS['ISPRO_IMG2PICTURE_MAX_IMG_WIDTH'] = 'Ширина изображения px';
+$MESS['ISPRO_IMG2PICTURE_TEMPLATE'] = 'Шаблон замены тега img';
+$MESS['ISPRO_IMG2PICTURE_USE_WEBP'] = 'Применить конвертацию изображений в webp';
+$MESS['ISPRO_IMG2PICTURE_IMG_COMPRESSION'] = 'Качество сохряняемых изображений (0-100)';
+$MESS['ISPRO_IMG2PICTURE_EXCEPTIONS_DIR'] = 'Исключения: каталоги (директории) сайта, где не будет раотать модуль';
+$MESS['ISPRO_IMG2PICTURE_EXCEPTIONS_SRC'] = 'Исключения: регулярные выражения для ссылок на изображения, которые не надо преобразовывать';
+$MESS['ISPRO_IMG2PICTURE_MODULE_MODE'] = 'Режим работы модуля';
+$MESS['ISPRO_IMG2PICTURE_MODULE_MODE_off'] = 'Выключен';
+$MESS['ISPRO_IMG2PICTURE_MODULE_MODE_test'] = 'Тестирование';
+$MESS['ISPRO_IMG2PICTURE_MODULE_MODE_on'] = 'Включен';
+$MESS['ISPRO_IMG2PICTURE_SAVE'] = 'Сохранить';
+$MESS['ISPRO_IMG2PICTURE_DEFAULT'] = 'Сбросить все настройки по умолчанию';
+$MESS['ISPRO_IMG2PICTURE_REMOVE_FILES'] = 'Отчистить кеш модуля и созданные файлы';
 
 $MESS['ISPRO_IMG2PICTURE_INFO'] = '
 Модуль заменяет теги img на теги picture с ресайзом и конвертацией изображений.
 <br>
+Модуль работает только с тегами img, у которых есть аттрибут src.
+<br>
+Модуль обрабатвает изображение из аттрибута src.
+<br>
 Модуль не работает если текущий пользователь адмиристратор - для того чтобы не заменять теги во время редактирования контента.
+<br>
+Модуль не работает в администаривной части сайта.
 <br>
 Т.е. тестирование работы модуля надо проводить не авторизованным пользователем.
 <br>
--- modules/is_pro.img2picture/options.php
+++ modules/is_pro.img2picture/options.php
@@ -24,34 +24,59 @@ $request = \Bitrix\Main\Application::getInstance()->getContext()->getRequest();
 $doc_root = \Bitrix\Main\Application::getDocumentRoot();
 $url_module = str_replace($doc_root, '', __DIR__);
 
-print_r_tree($arModuleCfg);
 $options_list = $arModuleCfg['options_list'];
 
+$ok_message = '';
+$eeror_message = '';
+if ($request->getpost('saveoptionsdefault') != '') {
+	include(__DIR__ . "/default_option.php");
+}
+
 foreach ($options_list as $option_name => $option_type) {
-	if ($request->getpost('saveoptions') != '') {
+	if (($request->getpost('saveoptions') != '') || ($request->getpost('saveoptionsdefault') != '')) {
 		$option[$option_name] = $request->getpost('option_' . $option_name);
 		if ($option_name == 'RESPONSIVE') {
 			foreach ($option[$option_name] as $key => $val) {
-				if ((trim($val['width']) == '') || (trim($val['max']) == '') || (trim($val['min']) == '')) {
+				if ((trim($val['width']) == '')) {
 					unset($option[$option_name][$key]);
-				}
-			}
-		}
+				};
+			};
+		};
 		if (is_array($option[$option_name])) {
 			$option[$option_name] = json_encode($option[$option_name]);
-		}
+		};
+		if ($request->getpost('saveoptionsdefault') != '') {
+			$option[$option_name] = $is_pro_img2picture_default_options[$option_name];
+		};
 		\Bitrix\Main\Config\Option::set($arModuleCfg['MODULE_ID'], $option_name, $option[$option_name]);
-		$message = new \CAdminMessage(array(
-			'MESSAGE' => 'SAVED: ' . $option_name . ' => ' . $option[$option_name],
-			'TYPE' => 'OK'
-		));
-		echo $message->Show();
+		$ok_message .= 'SAVED: ' . Loc::getMessage('ISPRO_IMG2PICTURE_' . $option_name) . PHP_EOL;
 	};
+
 	$option[$option_name] = \Bitrix\Main\Config\Option::get($arModuleCfg['MODULE_ID'], $option_name);
 	if ($option_type == 'json') {
 		$option[$option_name . '_VALUE'] = @json_decode($option[$option_name], true);
 	};
 };
+if ($request->getpost('removefiles') != '') {
+	include_once(__DIR__ . '/classes/main.class.php');
+	$img2picture = new \IS_PRO\img2picture\MainClass($option);
+	$img2picture->ClearDirCache();
+};
+if ($ok_message != '') {
+	$message = new \CAdminMessage(array(
+		'MESSAGE' => $ok_message,
+		'TYPE' => 'OK'
+	));
+	echo $message->Show();
+}
+if ($eeror_message != '') {
+	$message = new \CAdminMessage(array(
+		'MESSAGE' => $eeror_message,
+		'TYPE' => 'ERROR'
+	));
+	echo $message->Show();
+}
+
 $i = 0;
 while ($i < 5) {
 	$option['RESPONSIVE_VALUE'][] = [
@@ -62,7 +87,6 @@ while ($i < 5) {
 	$i++;
 }
 
-print_r_tree($option);
 $tabList = [
 	[
 		'DIV' => 'edit1',
@@ -75,7 +99,13 @@ $tabList = [
 
 $tabControl = new CAdminTabControl(str_replace('.', '_', $arModuleCfg['MODULE_ID']) . '_options', $tabList);
 ?>
-<form method="POST" action="<?= $currentUrl; ?>" enctype="multipart/form-data">
+<style>
+	#img2picture_form textarea {
+		width: 100%;
+		min-height: 150px;
+	}
+</style>
+<form method="POST" action="<?= $currentUrl; ?>" enctype="multipart/form-data" id="img2picture_form">
 	<?= bitrix_sessid_post(); ?>
 	<?
 	$tabControl->Begin();
@@ -91,32 +121,32 @@ $tabControl = new CAdminTabControl(str_replace('.', '_', $arModuleCfg['MODULE_ID
 			<?= EndNote(); ?>
 		</td>
 	</tr <tr>
-	<td style="text-align: left" width="20%" valign="top">
-		<?= Loc::getMessage('ISPRO_ING2PICTURE_RESPONSIVE') ?>
+	<td width="20%" valign="top">
+		<?= Loc::getMessage('ISPRO_IMG2PICTURE_RESPONSIVE') ?>
 	</td>
 	<td width="80%">
-		<table>
+		<table width="100%">
 			<tr>
 				<th>
-					<?= Loc::getMessage('ISPRO_ING2PICTURE_MIN_SCREEN_WIDTH') ?>
+					<?= Loc::getMessage('ISPRO_IMG2PICTURE_MIN_SCREEN_WIDTH') ?>
 				</th>
 				<th>
-					<?= Loc::getMessage('ISPRO_ING2PICTURE_MAX_SCREEN_WIDTH') ?>
+					<?= Loc::getMessage('ISPRO_IMG2PICTURE_MAX_SCREEN_WIDTH') ?>
 				</th>
 				<th>
-					<?= Loc::getMessage('ISPRO_ING2PICTURE_MAX_IMG_WIDTH') ?>
+					<?= Loc::getMessage('ISPRO_IMG2PICTURE_MAX_IMG_WIDTH') ?>
 				</th>
 			</tr>
 			<? foreach ($option['RESPONSIVE_VALUE'] as $key => $val) : ?>
 				<tr>
 					<td>
-						<input type="numeric" name="option_RESPONSIVE[<?= $key ?>][min]" value="<?= $val['min'] ?>">
+						<input type="number" name="option_RESPONSIVE[<?= $key ?>][min]" value="<?= $val['min'] ?>">
 					</td>
 					<td>
-						<input type="numeric" name="option_RESPONSIVE[<?= $key ?>][max]" value="<?= $val['max'] ?>">
+						<input type="number" name="option_RESPONSIVE[<?= $key ?>][max]" value="<?= $val['max'] ?>">
 					</td>
 					<td>
-						<input type="numeric" name="option_RESPONSIVE[<?= $key ?>][width]" value="<?= $val['width'] ?>">
+						<input type="number" name="option_RESPONSIVE[<?= $key ?>][width]" value="<?= $val['width'] ?>">
 					</td>
 				</tr>
 			<? endforeach ?>
@@ -126,7 +156,7 @@ $tabControl = new CAdminTabControl(str_replace('.', '_', $arModuleCfg['MODULE_ID
 
 	<tr>
 		<td>
-			<?= Loc::getMessage('ISPRO_ING2PICTURE_USE_WEBP') ?>
+			<?= Loc::getMessage('ISPRO_IMG2PICTURE_USE_WEBP') ?>
 		</td>
 		<td>
 			<input type="hidden" name="option_USE_WEBP" value="N" />
@@ -136,16 +166,16 @@ $tabControl = new CAdminTabControl(str_replace('.', '_', $arModuleCfg['MODULE_ID
 
 	<tr>
 		<td>
-			<?= Loc::getMessage('ISPRO_ING2PICTURE_IMG_COMPRESSION') ?>
+			<?= Loc::getMessage('ISPRO_IMG2PICTURE_IMG_COMPRESSION') ?>
 		</td>
 		<td>
-			<input type="numeric" name="option_IMG_COMPRESSION" value="<?= $option['IMG_COMPRESSION'] ?>" min="0" max="100" />
+			<input type="number" name="option_IMG_COMPRESSION" value="<?= $option['IMG_COMPRESSION'] ?>" min="0" max="100" />
 		</td>
 	</tr>
 
 	<tr>
 		<td>
-			<?= Loc::getMessage('ISPRO_ING2PICTURE_TEMPLATE') ?>
+			<?= Loc::getMessage('ISPRO_IMG2PICTURE_TEMPLATE') ?>
 		</td>
 		<td>
 			<textarea name="option_TEMPLATE"><?= $option['TEMPLATE'] ?></textarea>
@@ -154,7 +184,7 @@ $tabControl = new CAdminTabControl(str_replace('.', '_', $arModuleCfg['MODULE_ID
 
 	<tr>
 		<td>
-			<?= Loc::getMessage('ISPRO_ING2PICTURE_EXCEPTIONS_DIR') ?>
+			<?= Loc::getMessage('ISPRO_IMG2PICTURE_EXCEPTIONS_DIR') ?>
 		</td>
 		<td>
 			<textarea name="option_EXCEPTIONS_DIR"><?= $option['EXCEPTIONS_DIR'] ?></textarea>
@@ -163,7 +193,7 @@ $tabControl = new CAdminTabControl(str_replace('.', '_', $arModuleCfg['MODULE_ID
 
 	<tr>
 		<td>
-			<?= Loc::getMessage('ISPRO_ING2PICTURE_EXCEPTIONS_SRC') ?>
+			<?= Loc::getMessage('ISPRO_IMG2PICTURE_EXCEPTIONS_SRC') ?>
 		</td>
 		<td>
 			<textarea name="option_EXCEPTIONS_SRC"><?= $option['EXCEPTIONS_SRC'] ?></textarea>
@@ -172,15 +202,15 @@ $tabControl = new CAdminTabControl(str_replace('.', '_', $arModuleCfg['MODULE_ID
 
 	<tr>
 		<td>
-			<?= Loc::getMessage('ISPRO_ING2PICTURE_MODULE_MODE') ?>
+			<?= Loc::getMessage('ISPRO_IMG2PICTURE_MODULE_MODE') ?>
 		</td>
 		<td>
 
 			<select name="option_MODULE_MODE">
 				<? $arModuleMode = ['off', 'test', 'on']; ?>
 				<? foreach ($arModuleMode as $mode) : ?>
-					
-					<option value="<?= $mode ?>" <?=($option['MODULE_MODE'] == $mode ) ? 'selected': '' ?>><?= Loc::getMessage('ISPRO_ING2PICTURE_MODULE_MODE_' . $mode) ?></option>
+
+					<option value="<?= $mode ?>" <?= ($option['MODULE_MODE'] == $mode) ? 'selected' : '' ?>><?= Loc::getMessage('ISPRO_IMG2PICTURE_MODULE_MODE_' . $mode) ?></option>
 				<? endforeach ?>
 			</select>
 		</td>
@@ -188,10 +218,13 @@ $tabControl = new CAdminTabControl(str_replace('.', '_', $arModuleCfg['MODULE_ID
 
 	<tr>
 		<td colspan="2">
-			<input type="submit" class="adm-btn-save" name="saveoptions" value="<? echo Loc::getMessage('ISPRO_ING2PICTURE_SAVE'); ?>">
+
 		</td>
 	</tr>
 
 	<? $tabControl->Buttons(); ?>
+	<input type="submit" class="adm-btn-save" name="saveoptions" value="<? echo Loc::getMessage('ISPRO_IMG2PICTURE_SAVE'); ?>">
+	<input type="submit" class="adm-btn-save" name="saveoptionsdefault" value="<? echo Loc::getMessage('ISPRO_IMG2PICTURE_DEFAULT'); ?>">
+	<input type="submit" class="adm-btn-save" name="removefiles" value="<? echo Loc::getMessage('ISPRO_IMG2PICTURE_REMOVE_FILES'); ?>">
 	<? $tabControl->End(); ?>
 </form>
