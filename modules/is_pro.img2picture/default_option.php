6d2099a1bf5ce0e6aa14bbc964029e2643d342e4
Ivan Shabanov <ivan.shabanov@gmail.com>
2022-08-06 15:38:38 +0700

    feat: add exception src

--- modules/is_pro.img2picture/default_option.php
+++ modules/is_pro.img2picture/default_option.php
@@ -31,6 +31,5 @@ $is_pro_img2picture_default_options = [
 	'IMG_COMPRESSION' => '75',
 	'TEMPLATE' => file_get_contents(__DIR__.'/default_template.php'),
 	'EXCEPTIONS_DIR' => implode("\n", ['/bitrix/', '/auth/', '/personal/']),
-	'EXCEPTIONS_SRC' => implode("\n", ['http.*', '.*captcha.*', '.*\.php.*', '.*\?.*', '.*\.svg.*', 'data:.*']),
+	'EXCEPTIONS_SRC' => implode("\n", ['http.*', '\/\/.*','.*captcha.*', '.*\.php.*', '.*\?.*', '.*\.svg.*', 'data:.*']),
 ];
-?>
