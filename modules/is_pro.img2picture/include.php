5e3ac0fd97fd457db2e86df731ae3ff1356bf74e
Иван Шабанов <coder10@web-axioma.ru>
2022-08-09 10:42:49 +0700

    refactoring: functions GetOption, doIt

--- modules/is_pro.img2picture/include.php
+++ modules/is_pro.img2picture/include.php
@@ -23,19 +23,13 @@ class Main
 		if (\CSite::InDir('/bitrix/')) {
 			return;
 		};
-		include_once(__DIR__ . "/install/module.cfg.php");
-		$options_list = $arModuleCfg['options_list'];
-		foreach ($options_list as $option_name => $option_type) {
-			$option[$option_name] = \Bitrix\Main\Config\Option::get($arModuleCfg['MODULE_ID'], $option_name);
-			if ($option_type == 'json') {
-				$option[$option_name . '_VALUE'] = @json_decode($option[$option_name], true);
-			}
-		}
+		$option = self::GetOptions();
 		if ($option['MODULE_MODE'] == 'test') {
 			if ($_GET['img2picture']) {
 				$SESSION['img2picture'] = $_GET['img2picture'];
 			}
 			$option['MODULE_MODE'] = $SESSION['img2picture'];
+			$option['DEBUG'] = 'Y';
 		}
 		if ($option['MODULE_MODE'] !== 'on') {
 			return;
@@ -50,9 +44,27 @@ class Main
 				};
 			};
 		};
+		$content = self::doIt($content, $option);
+	}
+
+	public function doIt($content, $option)
+	{
 		include_once(__DIR__ . '/classes/main.class.php');
 		$option['DOCUMENT_ROOT'] = \Bitrix\Main\Application::getDocumentRoot();
 		$img2picture = new MainClass($option);
 		$img2picture->doIt($content);
+		return $content;
+	}
+
+	public function GetOptions() {
+		include_once(__DIR__ . "/install/module.cfg.php");
+		$options_list = $arModuleCfg['options_list'];
+		foreach ($options_list as $option_name => $option_type) {
+			$option[$option_name] = \Bitrix\Main\Config\Option::get($arModuleCfg['MODULE_ID'], $option_name);
+			if ($option_type == 'json') {
+				$option[$option_name . '_VALUE'] = @json_decode($option[$option_name], true);
+			}
+		}
+		return $option;
 	}
 }
