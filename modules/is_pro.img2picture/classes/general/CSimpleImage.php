37688a9f9f210259911541e9a0ce5512d5651af4
Иван Шабанов <coder10@web-axioma.ru>
2022-08-15 16:48:27 +0700

    fix: Paletter image not supported by webp

--- modules/is_pro.img2picture/classes/general/CSimpleImage.php
+++ modules/is_pro.img2picture/classes/general/CSimpleImage.php
@@ -23,14 +23,18 @@ class CSimpleImage
 		$this->image_type = $image_info[2];
 		if ($this->image_type == IMAGETYPE_JPEG) {
 			$this->image = imagecreatefromjpeg($filename);
+			imagepalettetotruecolor($this->image);
 		} elseif ($this->image_type == IMAGETYPE_GIF) {
 			$this->image = imagecreatefromgif($filename);
+			imagepalettetotruecolor($this->image);
 		} elseif ($this->image_type == IMAGETYPE_PNG) {
 			$this->image = imagecreatefrompng($filename);
+			imagepalettetotruecolor($this->image);
 			imagealphablending($this->image, false);
 			imagesavealpha($this->image, true);
 		} elseif ($this->image_type == IMAGETYPE_WEBP) {
 			$this->image = imagecreatefromwebp($filename);
+			imagepalettetotruecolor($this->image);
 			imagealphablending($this->image, false);
 			imagesavealpha($this->image, true);
 		} else {
