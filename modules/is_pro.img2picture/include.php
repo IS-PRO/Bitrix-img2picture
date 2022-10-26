40d970e01dd97a0762293b6f1e099341406c966b
Иван Шабанов <coder10@web-axioma.ru>
2022-10-26 10:12:13 +0700

    feat: для загрузки модуля применить Loader

--- modules/is_pro.img2picture/include.php
+++ modules/is_pro.img2picture/include.php
@@ -3,8 +3,8 @@ if (file_exists(__DIR__ . "/install/module.cfg.php")) {
 	include(__DIR__ . "/install/module.cfg.php");
 };
 
-CModule::IncludeModule($arModuleCfg['MODULE_ID']);
-global $DBType;
+use Bitrix\Main\Loader;
+Loader::includeModule($arModuleCfg['MODULE_ID']);
 
 $arClasses=array(
 	/* Библиотеки и классы для авто загрузки */
@@ -13,4 +13,4 @@ $arClasses=array(
 	'IS_PRO\img2picture\Cimg2picture'=>'classes/general/Cimg2picture.php'
 );
 
-CModule::AddAutoloadClasses($arModuleCfg['MODULE_ID'], $arClasses);
+Loader::registerAutoLoadClasses($arModuleCfg['MODULE_ID'], $arClasses);
