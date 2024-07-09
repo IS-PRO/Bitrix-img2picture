4d1e5eecbecdc7c408f52850eeb1d5d1b7a6fe93
Ivan Shabanov <Ivan.shabanov@gmail.com>
2024-07-09 16:28:29 +0700

    feat: remove self loader

--- modules/is_pro.img2picture/include.php
+++ modules/is_pro.img2picture/include.php
@@ -32,16 +32,11 @@ if (file_exists(__DIR__ . "/install/module.cfg.php")) {
 	include(__DIR__ . "/install/module.cfg.php");
 }
 
-use Bitrix\Main\Loader;
-
-Loader::includeModule($arModuleCfg['MODULE_ID']);
-
-
-$arClasses = array(
+$arClasses = [
 	/* Библиотеки и классы для авто загрузки */
-	'IS_PRO\img2picture\CSimpleImage' => 'classes/general/CSimpleImage.php',
+	'IS_PRO\img2picture\CSimpleImage'      => 'classes/general/CSimpleImage.php',
 	'IS_PRO\img2picture\CImageManupulator' => 'classes/general/CImageManupulator.php',
-	'IS_PRO\img2picture\Cimg2picture' => 'classes/general/Cimg2picture.php'
-);
+	'IS_PRO\img2picture\Cimg2picture'      => 'classes/general/Cimg2picture.php'
+];
 
-Loader::registerAutoLoadClasses($arModuleCfg['MODULE_ID'], $arClasses);
+\Bitrix\Main\Loader::registerAutoLoadClasses($arModuleCfg['MODULE_ID'], $arClasses);
